3ab80a6f3859b99c448b8bb2704ccf93378a94c6
CSBoggs <cboggs.dev@gmail.com>
2021-10-19 18:42:14 -0600

    added user POST endpoint

--- database/db_users.py
+++ database/db_users.py
@@ -22,6 +22,48 @@ def get_user_id(user_id):
         cursor.close()
         conn.close()
 
+def get_user_email(email):
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, username, email, birthdate, bio FROM users WHERE email = ?", [email])
+        
+        row_headers = [x[0] for x in cursor.description]
+        row_values = cursor.fetchall()
+
+        json_data=[]
+        for result in row_values:
+            json_data.append(dict(zip(row_headers, result)))
+        result = json_data
+        return result
+        
+    except mariadb.OperationalError:
+        print("something is wrong with the connection")
+    finally:
+        cursor.close()
+        conn.close()
+
+def get_user_username(username):
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, username, email, birthdate, bio FROM users WHERE username = ?", [username])
+        
+        row_headers = [x[0] for x in cursor.description]
+        row_values = cursor.fetchall()
+
+        json_data=[]
+        for result in row_values:
+            json_data.append(dict(zip(row_headers, result)))
+        result = json_data
+        return result
+        
+    except mariadb.OperationalError:
+        print("something is wrong with the connection")
+    finally:
+        cursor.close()
+        conn.close()
+
 def get_users_all():
     try:
         conn = connect_to_db()
@@ -40,5 +82,17 @@ def get_users_all():
     except mariadb.OperationalError:
         print("something is wrong with the connection")
     finally:
+        cursor.close()
+        conn.close()
+
+def create_user(username, email, password, birthdate, bio):
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, email, password, birthdate, bio) VALUES (?, ?, ?, ?, ?)", [username, email, password, birthdate, bio])
+        conn.commit()
+    except Exception as e:
+        print(e)
+    else:
         cursor.close()
         conn.close()
--- endpoints/users.py
+++ endpoints/users.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, Response, Blueprint
 from database import db_users
 import json
+from datetime import datetime
 
-users = Blueprint('/users', __name__)
+users = Blueprint('/api/users', __name__)
 
-@users.route('/users', methods=["GET"])
+@users.route('/api/users', methods=["GET"])
 def get_users():
     userId = None
 
     try: 
         userId = request.args['userId']
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(e)
     
     if userId:
         user = db_users.get_user_id(userId)
@@ -33,3 +34,32 @@ def get_users():
             resp = Response(
                 json.dumps(all, default=str), mimetype="application/json", status=200)
             return resp
+
+@users.route("/api/users", methods=["POST"])
+def create_user():
+    try:
+        data = request.get_json()
+        email = data["email"]
+        username = data["username"]
+        bio = data["bio"]
+        birthdate = datetime.strptime(data["birthdate"], "%Y-%m-%d")
+        password = data["password"]
+
+        try:
+            existing_user = db_users.get_user_email(email) or db_users.get_user_username(username)
+            if existing_user == True:
+                return Response(
+                    "Username and/or Email is in use", mimetype="text/plain", status=400
+                )
+        except:
+            pass
+    except:
+        return Response(
+            "Incorrect data provided", mimetype="text/plain", status=400
+        )
+    else:
+        db_users.create_user(username, email, password, birthdate, bio)
+        user = db_users.get_user_username(username)
+        resp = Response(
+            json.dumps(user, default=str), mimetype="application/json", status=201)
+        return resp        
