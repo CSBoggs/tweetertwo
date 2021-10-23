6eea5a7692aedf10885e99bf0d41376a083a3562
CSBoggs <cboggs.dev@gmail.com>
2021-10-23 12:35:28 -0600

    Added Tweet likes GET ALL and ID endpoints

--- database/db_tweet_likes.py
+++ database/db_tweet_likes.py
@@ -1,2 +1,9 @@
-import mariadb
-from .db_functions import connect_to_db, fetch
+from .db_functions import connect_to_db, fetch, insert
+
+def get_likes_tweet_id(tweet_id):
+    likes = fetch("SELECT tweetId, userId, username FROM (SELECT id, username FROM users) AS user JOIN (SELECT tweet_id AS tweetId, users_id AS userId FROM tweet_likes WHERE tweet_id = (?)) AS tweet_likes ON tweet_likes.userId = user.id", [tweet_id])
+    return likes
+
+def get_likes_all():
+    likes = fetch("SELECT tweetId, userId, username FROM (SELECT id, username FROM users) AS user JOIN (SELECT tweet_id as tweetId, users_id as userId FROM tweet_likes) AS tweet_likes ON tweet_likes.userId = user.id")
+    return likes
--- endpoints/tweet_likes.py
+++ endpoints/tweet_likes.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, jsonify, Response, request
-import dbcreds
+from flask import Blueprint, jsonify, make_response, Response, request
 from database import db_tweet_likes
 
 tweet_likes = Blueprint("/api/tweet-likes", __name__)
 
 @tweet_likes.route("/api/tweet-likes", methods=["GET"])
 def get_tweet_likes():
-    pass
+    tweet_id = None
+    if request.args:
+        tweet_id = request.args["tweetId"]
+    if tweet_id:
+        tweet_likes = db_tweet_likes.get_likes_tweet_id(tweet_id)
+        return make_response(jsonify(tweet_likes), 200)
+    else:
+        all = db_tweet_likes.get_likes_all()
+        if all:
+            return make_response(jsonify(all), 200)
+        else:
+            return make_response(jsonify({"message": "No likes found"}), 404)
